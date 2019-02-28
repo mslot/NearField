7a7c5af0af45d2c2bc256e1f8cb5acf8245aeaa6
Martin Slot <slot@sigsys.dk>
2019-02-28 23:02:28 +0100

    Initial commit

---
+++ src/NearField.TentacleServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NearField.TentacleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
