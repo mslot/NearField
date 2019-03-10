2ed5f33e4993f144439168d567449395f258dac6
Martin Slot <slot@sigsys.dk>
2019-03-10 10:22:46 +0100

    added deploy stage to build pipeline

--- tools/deploy_scripts/deploy_to_raspberry_pi.sh
+++ tools/deploy_scripts/deploy_to_raspberry_pi.sh
@@ -1,1 +1,11 @@
-echo "deploy with creds $1:$2&$3"
+# deploy code to staging
+echo "deploy with $1@$2:$3"
+
+staging_folder="$3_staging"
+rsync --delete -r ../../publish/* $1@$2:$staging_folder
+
+echo "code deployed successfully to staging folder"
+
+ssh $1@$2 sh $staging_folder/init_scripts/swap_staging_to_service_folder.sh
+
+echo "successfull activated new deploy"
---
+++ tools/init_scripts/swap_staging_to_service_folder.sh
@@ -0,0 +1,1 @@
+echo "Swapped code into service folder and activated service"
