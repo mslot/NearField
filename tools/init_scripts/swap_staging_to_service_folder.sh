43e4924ae9d5d8b27090d9d58c93b5c9067f3691
Martin Slot <slot@sigsys.dk>
2019-03-10 16:23:26 +0100

    Added prefix for services

--- tools/init_scripts/swap_staging_to_service_folder.sh
+++ tools/init_scripts/swap_staging_to_service_folder.sh
@@ -6,18 +6,18 @@ mkdir -p $2
 echo "created/modified $2"
 
 echo "copy service file to systemd/system folder"
-sudo -s cp $1/init_scripts/systemd_configs/management.api.service /etc/systemd/system/management.api.service
-sudo -s cp $1/init_scripts/systemd_configs/management.api.service /etc/systemd/system/tentacle.server.service
+sudo -s cp $1/init_scripts/systemd_configs/management.api.service /etc/systemd/system/nearfield.management.api.service
+sudo -s cp $1/init_scripts/systemd_configs/management.api.service /etc/systemd/system/nearfield.tentacle.server.service
 
 echo "stopping services"
-sudo -s systemctl stop management.api.service
-sudo -s systemctl stop tentacle.server.service
+sudo -s systemctl stop nearfield.management.api.service
+sudo -s systemctl stop nearfield.tentacle.server.service
 
 cp -r $1/. $2
 echo "copied $1 to $2"
 
 echo "starting services"
-sudo -s systemctl start management.api.service
-sudo -s systemctl start tentacle.server.service
+sudo -s systemctl start nearfield.management.api.service
+sudo -s systemctl start nearfield.tentacle.server.service
 
 echo "done swapping code into service folder and successfully activated service"
