f0510401837bb62502db89eee0f909b05b6a5b8a
Martin Slot <slot@sigsys.dk>
2019-03-10 15:53:28 +0100

    added deployment for management api

--- tools/deploy_scripts/deploy_to_raspberry_pi.sh
+++ tools/deploy_scripts/deploy_to_raspberry_pi.sh
@@ -1,11 +1,14 @@
-# deploy code to staging
 echo "deploy with $1@$2:$3"
 
 staging_folder="$3_staging"
+service_folder="$3_service"
+
+ssh -t $1@$2 "sudo mkdir -p $staging_folder; sudo mkdir -p $service_folder; sudo chown -R $1 $staging_folder; sudo chgrp -R $1 $service_folder; sudo chown -R $1 $service_folder; sudo chgrp -R $1 $staging_folder;"
+
 rsync --delete -r ../../publish/* $1@$2:$staging_folder
 
 echo "code deployed successfully to staging folder"
 
-ssh $1@$2 sh $staging_folder/init_scripts/swap_staging_to_service_folder.sh
+ssh -t $1@$2 sh $staging_folder/init_scripts/swap_staging_to_service_folder.sh $staging_folder $service_folder
 
-echo "successfull activated new deploy"
+echo "successful activated new deploy"
